d906d5d3b6f704560abfe15149353a3a38b7ed12
jp <83265356+jp-espresso@users.noreply.github.com>
2024-10-22 21:57:10 +1100

    add fb track

--- assets/variant-collections-add.js
+++ assets/variant-collections-add.js
@@ -1,17 +1,27 @@
 window.onload = function() {
   var quickAdd = document.getElementsByClassName('collection-variant-button ');
+  console.log(quickAdd)
   for (var i = 0; i < quickAdd.length; i++) {
     quickAdd[i].onclick = function(e) { 
       let variantId = e.target.getAttribute("data-variant-id");
       let buttonDiv = e.target.parentElement;
       let hiddenItemDiv = buttonDiv.nextElementSibling;
       hiddenItemDiv.setAttribute("value", variantId)
+      console.log(hiddenItemDiv);
       let formDiv = hiddenItemDiv.nextElementSibling;
+      console.log(formDiv.children)
       for (var j = 0; j < formDiv.children.length; j++) {
         if (formDiv.children[j].getAttribute("class") === "variant-id") {
           formDiv.children[j].setAttribute("value", variantId)
+          console.log(formDiv.children[j])
         }
       }
     }
   }
 }
+
+document.querySelector('form').onsubmit = e => {
+   e.target.submit();
+   e.target.reset();
+   return false;
+};
